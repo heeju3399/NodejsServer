696fd746a8d4c3b1f9156687a0eb30728b9994f2
origin <https://github.com/heeju3399/flutterExample.git>
2021-10-05 17:11:42 +0900

    json 회원 넣기 및 인덱스 회원 중복체크 및 회원 가입 구현

--- indexandroid.js
+++ indexandroid.js
@@ -1,46 +1,120 @@
 var express = require('express');
 const fs = require('fs');
 var app = express();
-var cors = require('cors');
 
+var cors = require('cors');
 app.use(cors());
 
-       
+app.post('/login', (req, res) => {
+    const fs = require('fs');
+    console.log('post login pass');
 
-app.post('/',(req,res) => {
-    console.log('post pass');
-    
     var flag = req.headers["flag"];
     var id = req.headers["id"];
     var pass = req.headers["pass"];
-    console.log(flag);
-    console.log(id);
-    console.log(pass);     
-    
+    var siteKey = req.headers["sitekey"];
+    console.log('flag : ' + flag);
+    console.log('id : ' + id);
+    console.log('pass : ' + pass);
+    console.log('sitekey : ' + siteKey);
     var resultTitle = 'no';
     var resultMessage = '아이디와 비밀번호가 맞지 않습니다';
-    
-    if(flag == ''){
+    if (siteKey == 'secretKey') {
+        console.log('secretKey ok');
+        if (flag == '') {
 
-    }else if(flag == 'signin'){
-        //로그인 되는지 안되는지 확인하고 넘겨주기 
-        const jsonFile = fs.readFileSync('./user.json', 'utf8');        
-        const jsonData = JSON.parse(jsonFile);
-        jsonData.users.forEach(element => {          
-            if(id === element.id && pass === element.pass){
-                resultTitle = 'pass';
-                resultMessage = 'ok';               
-            }                   
-        }); 
-    }else if(flag == 'signup'){
-        
+        } else if (flag == 'signIn') {
+            console.log('flag-ok');
+            //로그인 되는지 안되는지 확인하고 넘겨주기 
+
+            const jsonFile = fs.readFileSync('./user.json', 'utf8');
+            const jsonData = JSON.parse(jsonFile);
+            jsonData.users.forEach(element => {
+                if (id === element.id && pass === element.pass) {
+                    resultTitle = 'pass';
+                    resultMessage = 'ok';
+                }
+            });
+        } else if (flag == 'signup') {
+
+
+        }
+    } else {//사이트키가 다를때
+
     }
 
-    res.json({"title":resultTitle,"message":resultMessage});
+    res.json({ "title": resultTitle, "message": resultMessage });
 
 
-//    res.json({"key":"check header!"});
-    });
-    
-    
-app.listen(3000,() => console.log(3000));
+    //    res.json({"key":"check header!"});
+});
+///////////////////////////////////////////////////////////
+//회원가입 페이지////회원가입 페이지////회원가
+//입 페이지////회원가입 페이지//
+app.post('/signup', (req, res) => {
+    const fs = require('fs');
+    console.log('post signup pass');
+    var flag = req.headers["flag"];
+    var id = req.headers["id"];
+    var pass = req.headers["pass"];
+    var name = req.headers["name"];
+    var siteKey = req.headers["sitekey"];
+    console.log('flag : ' + flag);
+    console.log('id : ' + id);
+    console.log('pass : ' + pass);
+    console.log('name : ' + name);
+    console.log('sitekey : ' + siteKey);
+
+    var resultTitle = 'no';
+    var resultMessage = '뭔가 모를 에러입니다';
+    var doubleCheck = false;
+    if (siteKey == 'secretKey') {//사이트키확인
+        console.log('secretKey ok');
+        if (flag == '') {//플레이그 확인
+
+        } else if (flag == 'signup') {
+            console.log('flag-ok');
+            //로그인 되는지 안되는지 확인하고 넘겨주기 
+            //회원가입 페이지////회원가입 페이지////회원가입 페이지////회원가입 페이지////회원가입 페이지//
+            //회원가입 페이지////회원가입 페이지////회원가입 페이지////회원가입 페이지////회원가입 페이지//
+            const jsonFile = fs.readFileSync('./user.json', 'utf8');
+            var jsonData = JSON.parse(jsonFile);
+
+            jsonData.users.forEach(element => {
+
+                if (id === element.id) {
+                    resultTitle = 'doubleCheck';
+                    resultMessage = '아이디가 중복 되었습니다';
+                    doubleCheck = true;
+
+                }
+            });
+            //signUp
+
+            if (!doubleCheck) {
+                jsonData['users'].push({ "id": id, "name": name, "pass": pass });
+                const jsonFinalData = JSON.stringify(jsonData);
+                fs.writeFileSync('./user.json', jsonFinalData, 'utf8', function (err) {
+                    console.log('write err:' + err);
+                });
+                resultTitle = 'pass';
+                resultMessage = 'ok';
+            }
+
+        } else {//플래그 다를떄
+            resultTitle = 'diffrent flag';
+            resultMessage = 'check flag';
+        }
+    } else {//사이트키가 다를때
+        //접속금지 
+        resultTitle = 'who are you?';
+        resultMessage = 'calling 911';
+    }
+    console.log('result rirle : ' + resultTitle + ' // result message ' + resultMessage);
+    res.json({ "title": resultTitle, "message": resultMessage });
+
+});
+//회원가입 페이지////회원가입 페이지////회원가입 페이지////회원가입 페이지//
+//회원가입 페이지////회원가입 페이지////회원가입 페이지////회원가입 페이지//
+
+app.listen(3000, () => console.log(3000));
