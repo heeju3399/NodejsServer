51baf660cda2cbf6224c80d27ef27e12c1637a36
origin <https://github.com/heeju3399/flutterExample.git>
2021-10-06 22:10:44 +0900

    글쓰기 구현

--- indexandroid.js
+++ indexandroid.js
@@ -117,4 +117,56 @@ app.post('/signup', (req, res) => {
 //회원가입 페이지////회원가입 페이지////회원가입 페이지////회원가입 페이지//
 //회원가입 페이지////회원가입 페이지////회원가입 페이지////회원가입 페이지//
 
+app.post('/setcontent', (req, res) => {
+    const fs = require('fs');
+    console.log('post setcontent pass');
+    var flag = req.headers["flag"];
+    var id = req.headers["id"];
+    var content = req.headers["content"];
+    var siteKey = req.headers["sitekey"];
+    var nowtime = req.headers["nowtime"];
+    var visible = req.headers["visible"];
+
+    console.log('id : ' + id);
+    console.log('content : ' + content);
+    console.log('nowtime : ' + nowtime);
+    console.log('visible : ' + visible);
+
+    ///////////////////////////////////////this///////////////////////////////////////
+    var resultTitle = 'no';
+    var resultMessage = '뭔가 모를 에러입니다';
+    if (siteKey == 'secretKey') {//사이트키확인
+        console.log('secretKey ok');
+        if (flag == '') {//플레이그 확인
+        } else if (flag == 'signup') {
+            console.log('flag-ok');
+
+            const jsonFile = fs.readFileSync('./content.json', 'utf8');
+            var jsonData = JSON.parse(jsonFile);
+
+
+            jsonData['users'].push({ "id": id, "name": name, "pass": pass });
+            const jsonFinalData = JSON.stringify(jsonData);
+            fs.writeFileSync('./user.json', jsonFinalData, 'utf8', function (err) {
+                console.log('write err:' + err);
+            });
+            resultTitle = 'pass';
+            resultMessage = 'ok';
+
+
+        } else {//플래그 다를떄
+            resultTitle = 'diffrent flag';
+            resultMessage = 'check flag';
+        }
+    } else {//사이트키가 다를때
+        //접속금지 
+        resultTitle = 'who are you?';
+        resultMessage = 'calling 911';
+    }
+    console.log('result rirle : ' + resultTitle + ' // result message ' + resultMessage);
+    res.json({ "title": resultTitle, "message": resultMessage });
+
+});
+
+
 app.listen(3000, () => console.log(3000));
